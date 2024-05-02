2e428fda6e84813136e959be54223c4eef7be56d
madzera <diegomadson@msn.com>
2024-05-02 23:19:40 +0100

    Change the way that the price data is retrieved from the WebService

--- lib/main.dart
+++ lib/main.dart
@@ -39,49 +39,81 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(32),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("assets/images/bitcoin.png"),
-              Padding(
-                padding: const EdgeInsets.only(top: 30, bottom: 30),
-                child: Text(
-                  _price,
-                  style: const TextStyle(
-                    fontSize: 35
-                  ),
-                ),
-              ),
-              MaterialButton(
-                color: Colors.orange,
-                padding: const EdgeInsets.fromLTRB(30, 15, 30, 15),
-                onPressed: _retrievePrice,
-                child: const Text(
-                  "Atualizar",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+   return Scaffold(
+     body: Container(
+       padding: const EdgeInsets.all(32),
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+           Image.asset("assets/images/bitcoin.png"),
+           Padding(
+             padding: const EdgeInsets.only(top: 30, bottom: 30),
+             child: _getFutureBuilder()
+           ),
+           MaterialButton(
+             color: Colors.orange,
+             padding: const EdgeInsets.fromLTRB(30, 15, 30, 15),
+             onPressed: _retrievePrice,
+             child: const Text(
+               "Atualizar",
+               style: TextStyle(
+                 fontSize: 20,
+                 color: Colors.white
+               ),
+             ),
+           )
+
+         ],
+       ),
+     ),
+   );
   }
 
-  void _retrievePrice() async {
+  Future<String> _retrievePrice() async {
     http.Response response = await http.get(Uri.parse(_endpoint));
     Map<String, dynamic> result = json.decode(response.body);
     double value = result["BRL"]["buy"];
 
-    setState(() {
-      _price = CurrencyFormatter.parse(value);
-    });
+    _price = CurrencyFormatter.parse(value);
+    return _price;
+  }
+
+  FutureBuilder _getFutureBuilder() {
+    double initialPrice = 0;
+    return FutureBuilder(
+      initialData: Text(CurrencyFormatter.parse(initialPrice), style: const TextStyle(fontSize: 35)),
+      future: _retrievePrice(),
+      builder: (context, snapshot) {
+        ConnectionState connectionState = snapshot.connectionState;
+
+        String price = '';
+        switch (connectionState) {
+          case ConnectionState.done: {
+            if (snapshot.hasError) {
+              price = CurrencyFormatter.parse(initialPrice);
+            } else {
+              price = snapshot.data;
+            }
+            break;
+          }
+          case ConnectionState.active: {
+            break;
+          }
+          case ConnectionState.waiting: {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          case ConnectionState.none: {
+            break;
+          }
+        }
+
+        return Text(
+          price,
+          style: const TextStyle(fontSize: 35),
+        );
+      },
+    );
   }
 }
